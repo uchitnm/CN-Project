e832cf6ad193b53ef8e30195b613f13ca34e06b2
uchitnm <uchitnm@gmail.com>
2024-02-20 23:21:51 +0530

    added condition checking in server

--- client.py
+++ client.py
@@ -2,20 +2,20 @@ import socket
 import threading
 import json
 
-
-
 def enter_server():
-
+    
     with open('servers.json', ) as f:
         data = json.load(f)
     print("Rooms Avaliable: ")
     for servers in data:
         print(servers, end=" ")
+    # Ask user for the name of the server to join
 
     server_name = input("\nEnter the server name:")
     global nickname
     nickname = input("Choose Your Nickname:")
 
+    # Store the ip and port number for connection
     ip = data[server_name]["ip"]
     port = data[server_name]["port"]
     
@@ -25,7 +25,7 @@ def enter_server():
     client.connect((ip, port))
 
 
-
+# Menu loop, it will loop until the user choose to enter a server
 while True:
     enter_server()
     break
@@ -56,8 +56,9 @@ def write():
     while True:
         if stop_thread:
             break
-
+        # Getting Messages
         message = f'{nickname}: {input("")}'
+
         client.send(message.encode('ascii'))
 
 
@@ -65,4 +66,3 @@ receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 write_thread = threading.Thread(target=write)
 write_thread.start()
-
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import threading
 import socket
 
 ip = "127.0.0.1"
-port = [5500, 5550, 5000, 5555]
+port = [1234,1235,1236,1237]
 server_sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(4)]
 
 for i, server_socket in enumerate(server_sockets):
@@ -14,7 +14,6 @@ for i, server_socket in enumerate(server_sockets):
 class ServerConnect:
     def __init__(self, server):
         self.server = server
-
         self.clients = {}
 
     def broadcast(self, message):
@@ -35,12 +34,22 @@ class ServerConnect:
 
     def receive(self):
         while True:
+
             client, address = self.server.accept()
-            print(f"Connected with {str(address)}")
+            if len(self.clients) == 5 :
+                client.send("Room Full".encode('ascii'))
+                break
+
             client.send('NICK'.encode('ascii'))
             nickname = client.recv(1024).decode('ascii')
+            
+            if nickname in self.clients.values():
+                client.send("Username Present".encode('ascii'))
+                break
 
-            self.clients[client] = {"nickname" : nickname }
+            print(f"Connected with {str(address)}")
+
+            self.clients[client] = nickname 
             
             print(f'Nickname of the client is {nickname}')
             self.broadcast(f'{nickname} joined the Chat'.encode('ascii'))
