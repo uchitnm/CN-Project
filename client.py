853620aeffa0b1ed2ae12ab4ad52ea4af98ccb5a
uchitnm <uchitnm@gmail.com>
2024-02-21 12:29:52 +0530

    5 conncetions and username checking

--- client.py
+++ client.py
@@ -1,17 +1,16 @@
 import socket
 import threading
 import json
+import sys
 
 def enter_server():
-    
     with open('servers.json', ) as f:
         data = json.load(f)
     print("Rooms Avaliable: ")
     for servers in data:
-        print(servers, end=" ")
+        print(servers)
     # Ask user for the name of the server to join
-
-    server_name = input("\nEnter the server name:")
+    server_name = input("Enter the server name:")
     global nickname
     nickname = input("Choose Your Nickname:")
 
@@ -26,6 +25,7 @@ def enter_server():
 
 
 # Menu loop, it will loop until the user choose to enter a server
+
 while True:
     enter_server()
     break
@@ -35,32 +35,68 @@ while True:
 stop_thread = False
 
 
+
 def receive():
+    local_name = ""
     while True:
         global stop_thread
         if stop_thread:
             break
         try:
             message = client.recv(1024).decode('ascii')
+            
+            if message == "Room Full":
+                print(message)
+                raise socket.error
+
+            if message == "Username Present":
+                print(message)
+                sys.stdout.flush()  # Flush the output buffer
+                # Ask the user to choose a different nickname
+                global nickname
+                nickname=""
+                nickname = input("Choose a different nickname:")
+                # Retry sending the nickname
+                client.send(nickname.encode('ascii'))
+                continue
+
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
             else:
                 print(message)
-        except socket.error:
-            print('Error Occured while Connecting')
-            client.close()
-            break
 
+        except socket.error:
+            print('Error Occurred while Connecting')
+            client.close()
+            stop_thread = True
+            exit(0)
+            break
 
 def write():
+    global stop_thread
     while True:
-        if stop_thread:
+        try:
+
+            if stop_thread:
+                break
+            # Getting Messages
+            if nickname!="":
+                message = f'{nickname}: {input("")}'
+
+            if "/leave" in message:
+                    client.send(message.encode("ascii"))
+                    print("Your left the server.")
+                    client.close()
+                    stop_thread = True
+                    break
+            if(nickname!=""):
+                client.send(message.encode('ascii'))
+
+        except socket.error:
+            print('Error Occurred while Sending Message')
+            client.close()
+            stop_thread = True
             break
-        # Getting Messages
-        message = f'{nickname}: {input("")}'
-
-        client.send(message.encode('ascii'))
-
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
--- server.py
+++ server.py
@@ -2,13 +2,12 @@ import threading
 import socket
 
 ip = "127.0.0.1"
-port = [1234,1235,1236,1237]
+port = [49153,49154,49155,49156]
 server_sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(4)]
 
 for i, server_socket in enumerate(server_sockets):
     server_socket.bind((ip, port[i]))
     server_socket.listen()
-
 
 
 class ServerConnect:
@@ -24,31 +23,40 @@ class ServerConnect:
         while True:
             try:
                 message = client.recv(1024)
+                if "/leave" in message.decode("ascii") :
+                        self.broadcast(f'{self.clients[client]} left the Chat!'.encode('ascii'))
+                        print(f'{self.clients[client]} left the Chat!')
+                        del self.clients[client]
+                        break
                 self.broadcast(message)  
 
             except socket.error:
                 if client in self.clients:
-                        self.broadcast(f'{self.clients[client]["nickname"]} left the Chat!'.encode('ascii'),client)
+                        self.broadcast(f'{self.clients[client]} left the Chat!'.encode('ascii'))
                         del self.clients[client]
                         break
 
-    def receive(self):
+    def receive_connection(self):
         while True:
 
             client, address = self.server.accept()
-            if len(self.clients) == 5 :
+
+            if len(self.clients) == 2 :
                 client.send("Room Full".encode('ascii'))
-                break
+                continue
 
             client.send('NICK'.encode('ascii'))
             nickname = client.recv(1024).decode('ascii')
-            
-            if nickname in self.clients.values():
+
+            while nickname in self.clients.values():
+                print(self.clients)
+                nickname = ""
                 client.send("Username Present".encode('ascii'))
-                break
+                nickname = client.recv(1024).decode('ascii')
+                
 
             print(f"Connected with {str(address)}")
-
+            
             self.clients[client] = nickname 
             
             print(f'Nickname of the client is {nickname}')
@@ -64,7 +72,7 @@ print('Server is Listening ...')
 servers = [ServerConnect(server_socket) for server_socket in server_sockets]
 
 
-server_threads = [threading.Thread(target=server.receive) for server in servers]
+server_threads = [threading.Thread(target=server.receive_connection) for server in servers]
 for thread in server_threads:
     thread.start()
 
